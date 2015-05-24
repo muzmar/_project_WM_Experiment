11fa6f08075ea86fb6f065f5bb84ab5e49453ad4
Jose <hosseinsabri@gmail.com>
2015-05-24 03:08:20 +0200

    load update, load arrow, arranging class, functions TODO: reaction Time, page between each quarter of experiment

--- __game_WM.py
+++ __game_WM.py
@@ -13,153 +13,20 @@ from psychopy import visual, core, event, gui, data
 # ===============================================================================
 # global variables: INTERFACE
 # ===============================================================================
-
 PATH = 'C:\\pythonProjects\\_project_WM_Experiment'
-FIXCROSS_SIZE = 40  # size of the fixation cross (the character '+' in Arial)
-INSTR_CHAR_SIZE = 18  # character size for instructions
 OUTPATH = '%s\\results\\' % PATH  # output path for storing the results
 LANGUAGE = 'DE'  # which language is the experiment in: 'DE'=German. 'CN'=Chinese
-SCREEN_SIZE = [GetSystemMetrics(0), GetSystemMetrics(1)]  # what is your screen resolution?
-# SCREEN_SIZE = [800, 480]  # what is your screen resolution?
-LANG_FONT_MAP = {'DE': 'Courier New', 'CN': 'SimSun'}  # what font is used for what language?
-HALF_WIDTH = 432/8
+# SCREEN_SIZE = [GetSystemMetrics(0), GetSystemMetrics(1)]  # what is your screen resolution?
+SCREEN_SIZE = [1000,600]
+HALF_WIDTH = int(round((SCREEN_SIZE[0]/7.942)/2))
+rtClock = core.Clock()  # reaction time clock
 
-# ===============================================================================
-# initial dialogue box
-# ===============================================================================
-inputInfo = ['a', '']
-count = 0
-while inputInfo[0] != inputInfo[1] or not inputInfo[0].isdigit():
-    expDlg = gui.Dlg(title="WM Experiment")
-    expDlg.addText("Subject info")
-    # expDlg.addField("Name:", fieldLength=25)
-    expDlg.addField("Subject_ID:")
-    expDlg.addField("Repeat__ID:")
-    expDlg.addText("")
-    # expDlg.addText("Experiment info")
-    # expDlg.addField("Group:", choices=["Test", "Control"], fieldLength=25)
-    # expDlg.addText("")
-    if count:
-        expDlg.addText("ERROR: IDs must be same integers", color='Red')
-    expDlg.show()
-    count += 1
-    if expDlg.OK:
-        inputInfo = expDlg.data
-    else:
-        core.quit()
-
-# ===============================================================================
-# read stimuli
-# ===============================================================================
-
-
-def read_stimuli(stimuli):
-    trials = []
-    file_header = []
-    with codecs.open(stimuli, 'rb', encoding="utf-8") as infile:
-        for line in infile:
-            line = line.strip()
-            if '###' in line:  # its the header
-                line = line.split(';')
-                file_header.append(line[0:len(line)-1])
-            elif len(line) == 0:  # last line if an empty one
-                break
-            else:
-                line = line.split(';')
-                trials.append(line[0:len(line)-1])  # write entire rest of the line
-    return trials, file_header
-
-trials_list, header = read_stimuli('%s\\%s\\stimuli\\Trials_WMExperiment_%s.txt'%(PATH,LANGUAGE,LANGUAGE))
-
-# Edit header line for using in the output
-header[0][0] = "trial"
-header[0].insert(0, "load")
-header[0].insert(0, "Subject_ID")
-header[0].append("question1")
-header[0].append("rt1")
-header[0].append("question2")
-header[0].append("rt2")
-header[0].append("question3")
-header[0].append("rt3")
 
 # ===============================================================================
 # Other preparations
 # ===============================================================================
 
-# expInfo = {'subjectName':inputInfo[0], 'subjectID':inputInfo[1], 'group':inputInfo[-1], "expName":"WM Experiment"}
-expInfo = {'subjectID': inputInfo[1], 'expName': "WM_Experiment"}
-
-output_file = OUTPATH + expInfo['expName'] + '_%s.txt' % expInfo['subjectID']
-rtClock = core.Clock()  # reaction time clock
-
-# create a window
-expWindow = visual.Window(size=SCREEN_SIZE, monitor="testMonitor", color=(230,230,230), fullscr=True,
-                          colorSpace='rgb255', units=u'pix')
-
-correct_answer_message = visual.TextStim(expWindow, pos=[0, 0], text="Richtig!", font='Courier New', bold=True,
-                                         color=(0, 1.0, 0), height=50, alignHoriz='center', units=u'pix')
-false_answer_message = visual.TextStim(expWindow, pos=[0, 0], text="Falsch!", font='Courier New', bold=True,
-                                       color=(1.0, 0, 0), height=50, alignHoriz='center', units=u'pix')
-time_up_message = visual.TextStim(expWindow, pos=[0, 0], text="Die Zeit ist um!", font='Courier New', bold=True,
-                                  color=(1.0, 0, 0), height=35, alignHoriz='center', units=u'pix')
-
-# ===============================================================================
-# check answer
-# ===============================================================================
-
-
-def check_answer(x, y, correct_answer):
-
-    # :param x: integer first dimension of mouse click
-    # :param y: integer second dimension of mouse click
-    # :param correct_answer: string
-    # :return:
-
-    interval = []
-    if correct_answer == "A1":
-        interval = [-4*HALF_WIDTH, -2*HALF_WIDTH, -4*HALF_WIDTH, -2*HALF_WIDTH]
-    elif correct_answer == "A2":
-        interval = [-2*HALF_WIDTH, 0, -4*HALF_WIDTH, -2*HALF_WIDTH]
-    elif correct_answer == "A3":
-        interval = [0, 2*HALF_WIDTH, -4*HALF_WIDTH, -2*HALF_WIDTH]
-    elif correct_answer == "A4":
-        interval = [2*HALF_WIDTH, 4*HALF_WIDTH, -4*HALF_WIDTH, -2*HALF_WIDTH]
-    elif correct_answer == "B1":
-        interval = [-4*HALF_WIDTH, -2*HALF_WIDTH, -2*HALF_WIDTH, 0]
-    elif correct_answer == "B2":
-        interval = [-2*HALF_WIDTH, 0, -2*HALF_WIDTH, 0]
-    elif correct_answer == "B3":
-        interval = [0, 2*HALF_WIDTH, -2*HALF_WIDTH, 0]
-    elif correct_answer == "B4":
-        interval = [2*HALF_WIDTH, 4*HALF_WIDTH, -2*HALF_WIDTH, 0]
-    elif correct_answer == "C1":
-        interval = [-4*HALF_WIDTH, -2*HALF_WIDTH, 0, 2*HALF_WIDTH]
-    elif correct_answer == "C2":
-        interval = [-2*HALF_WIDTH, 0, 0, 2*HALF_WIDTH]
-    elif correct_answer == "C3":
-        interval = [0, 2*HALF_WIDTH, 0, 2*HALF_WIDTH]
-    elif correct_answer == "C4":
-        interval = [2*HALF_WIDTH, 4*HALF_WIDTH, 0, 2*HALF_WIDTH]
-    elif correct_answer == "D1":
-        interval = [-4*HALF_WIDTH, -2*HALF_WIDTH, 2*HALF_WIDTH, 4*HALF_WIDTH]
-    elif correct_answer == "D2":
-        interval = [-2*HALF_WIDTH, 0, 2*HALF_WIDTH, 4*HALF_WIDTH]
-    elif correct_answer == "D3":
-        interval = [0, 2*HALF_WIDTH, 2*HALF_WIDTH, 4*HALF_WIDTH]
-    elif correct_answer == "D4":
-        interval = [2*HALF_WIDTH, 4*HALF_WIDTH, 2*HALF_WIDTH, 4*HALF_WIDTH]
-
-    if (interval[0] <= x <= interval[1]) and (interval[2] <= y <= interval[3]):
-        return 1
-    else:
-        return 0
-
-# ===============================================================================
-# experiment class
-# ===============================================================================
-
-
-class Image():
+class Image():  # class that creates ....
 
     def __init__(self, name, _type, **kwargs):
 
@@ -180,9 +47,9 @@ class Image():
         if self.type == "arrow":
             position = [0, 0]
         elif self.type == "questionMark":
-            position = [-216-54, 216-75]
+            position = [-4*HALF_WIDTH-50-80, 4*HALF_WIDTH-106]
         elif self.type == "questionLoad":
-            position = [-216-54, -34]
+            position = [-4*HALF_WIDTH-50-80, -75]
         elif self.type == "load":
             if self.loc == "D1":
                 position = [-3*HALF_WIDTH, -3*HALF_WIDTH]
@@ -224,14 +91,118 @@ class Image():
         buffer_image = visual.ImageStim(expWindow, image=self.path, pos=self.get_position(), units=u'pix')
         return buffer_image
 
-# -----------------------------------------------------------------------------
-# define trial procedure
+# ===============================================================================
+# Functions
+# ===============================================================================
+
+
+def read_stimuli(stimuli):
+    trials = []
+    file_header = []
+    with codecs.open(stimuli, 'rb', encoding="utf-8") as infile:
+        for line in infile:
+            line = line.strip()
+            if '###' in line:  # its the header
+                line = line.split(';')
+                file_header.append(line[0:len(line)-1])
+            elif len(line) == 0:  # last line if an empty one
+                break
+            else:
+                line = line.split(';')
+                trials.append(line[0:len(line)-1])  # write entire rest of the line
+    return trials, file_header
+
+
+def check_answer(x, y, correct_answer):
+
+    # :param x: integer first dimension of mouse click
+    # :param y: integer second dimension of mouse click
+    # :param correct_answer: string
+    # :return:
+
+    interval = []
+    if correct_answer == "D1":
+        interval = [-4*HALF_WIDTH, -2*HALF_WIDTH, -4*HALF_WIDTH, -2*HALF_WIDTH]
+    elif correct_answer == "D2":
+        interval = [-2*HALF_WIDTH, 0, -4*HALF_WIDTH, -2*HALF_WIDTH]
+    elif correct_answer == "D3":
+        interval = [0, 2*HALF_WIDTH, -4*HALF_WIDTH, -2*HALF_WIDTH]
+    elif correct_answer == "D4":
+        interval = [2*HALF_WIDTH, 4*HALF_WIDTH, -4*HALF_WIDTH, -2*HALF_WIDTH]
+    elif correct_answer == "C1":
+        interval = [-4*HALF_WIDTH, -2*HALF_WIDTH, -2*HALF_WIDTH, 0]
+    elif correct_answer == "C2":
+        interval = [-2*HALF_WIDTH, 0, -2*HALF_WIDTH, 0]
+    elif correct_answer == "C3":
+        interval = [0, 2*HALF_WIDTH, -2*HALF_WIDTH, 0]
+    elif correct_answer == "C4":
+        interval = [2*HALF_WIDTH, 4*HALF_WIDTH, -2*HALF_WIDTH, 0]
+    elif correct_answer == "B1":
+        interval = [-4*HALF_WIDTH, -2*HALF_WIDTH, 0, 2*HALF_WIDTH]
+    elif correct_answer == "B2":
+        interval = [-2*HALF_WIDTH, 0, 0, 2*HALF_WIDTH]
+    elif correct_answer == "B3":
+        interval = [0, 2*HALF_WIDTH, 0, 2*HALF_WIDTH]
+    elif correct_answer == "B4":
+        interval = [2*HALF_WIDTH, 4*HALF_WIDTH, 0, 2*HALF_WIDTH]
+    elif correct_answer == "A1":
+        interval = [-4*HALF_WIDTH, -2*HALF_WIDTH, 2*HALF_WIDTH, 4*HALF_WIDTH]
+    elif correct_answer == "A2":
+        interval = [-2*HALF_WIDTH, 0, 2*HALF_WIDTH, 4*HALF_WIDTH]
+    elif correct_answer == "A3":
+        interval = [0, 2*HALF_WIDTH, 2*HALF_WIDTH, 4*HALF_WIDTH]
+    elif correct_answer == "A4":
+        interval = [2*HALF_WIDTH, 4*HALF_WIDTH, 2*HALF_WIDTH, 4*HALF_WIDTH]
+
+    if (interval[0] < x < interval[1]) and (interval[2] < y < interval[3]):
+        return 1
+    else:
+        return 0
+
+
+def update_direction_finder(dir):
+
+    if dir == "N":
+        return 0
+    elif dir == "NW":
+        return 45
+    elif dir == "W":
+        return 90
+    elif dir == "SW":
+        return 135
+    elif dir == "S":
+        return 180
+    elif dir == "SO":
+        return 225
+    elif dir == "O":
+        return 270
+    elif dir == "NO":
+        return 315
+
+
+def load_background(): # Load background fit to expWindow
+    bg = Image("Background", "arrow").buffer()
+    bg.setSize(SCREEN_SIZE[0]/3150.0, '*')
+    return bg.draw()
+
+
+def load_update_arrow(kind, direction):
+
+    arrow = Image("A_" + kind, "arrow").buffer()
+    arrow.ori = update_direction_finder(direction)
+    return arrow.draw()
+
+
+def load_small_cartoon(color):
+
+    small_cartoon = Image("Nadel_" + color, "arrow").buffer()
+    return small_cartoon.draw()
 
 
 def run_trials(items, practice=False):
 
     # loop through trials
-    for i in range(len(items)):  # trial_order:
+    for i in [0,4]:  # range(len(items)):  # trial_order:
 
         item = items[i]  # -1]
 
@@ -243,7 +214,6 @@ def run_trials(items, practice=False):
             out_item.insert(1, "2")
 
         # prepare stimulus and draw on screen
-        background = Image("background", "arrow").buffer()
         cartoon1 = Image(item[1], "load", loc=item[2]).buffer()
         cartoon2 = Image(item[3], "load", loc=item[4]).buffer()
 
@@ -252,7 +222,7 @@ def run_trials(items, practice=False):
         core.wait(1.5)  # 1500 ms
 
         # Initial view
-        background.draw()
+        load_background()
         cartoon1.draw()
         cartoon2.draw()
         if item[5]:
@@ -268,10 +238,9 @@ def run_trials(items, practice=False):
         for jj in range(4):
             if jj == 3 and not item[5]:
                 break
-            Image("background", "arrow").buffer().draw()
-            update = Image(item[jj+7], "arrow").buffer()
-            update.ori = 45
-            update.draw()
+            load_background()
+            load_update_arrow(item[7+jj*2], item[8+jj*2])
+            load_small_cartoon(item[7+jj*2])
             expWindow.flip()
             core.wait(2.5)  # 2500 ms
 
@@ -279,15 +248,15 @@ def run_trials(items, practice=False):
             expWindow.flip()
             core.wait(.5)  # 500 ms
 
-        # questions ---------------------- now we have one column for update (it should get modified for two)
+        # questions
         for ii in range(3):
             if ii == 2 and not item[5]:  # check that the trial is two loads or three loads
                 for j in range(2):
                     item.append("")
                 break
-            Image("background", "arrow").buffer().draw()
-            Image("questionMark", "questionMark").buffer().draw()
-            Image(item[11+ii*2], "questionLoad").buffer().draw()
+            load_background()
+            Image("Question", "questionMark").buffer().draw()
+            Image(item[15+ii*2], "questionLoad").buffer().draw()
 
             # mouse interaction
             mouse = event.Mouse(win=expWindow)
@@ -300,9 +269,9 @@ def run_trials(items, practice=False):
                     answer = 0
                     rt = 31
                     if practice:
-                        Image("background", "arrow").buffer().draw()
-                        Image("questionMark", "questionMark").buffer().draw()
-                        Image(item[11+ii*2], "questionLoad").buffer().draw()
+                        load_background()
+                        Image("Question", "questionMark").buffer().draw()
+                        Image(item[15+ii*2], "questionLoad").buffer().draw()
                         time_up_message.draw()
                         expWindow.flip()
                         core.wait(1)
@@ -312,12 +281,12 @@ def run_trials(items, practice=False):
                     break
                 if mouse.getPressed()[0]:
                     x, y = mouse.getPos()
-                    answer = check_answer(x, y, item[12+ii*2])
+                    answer = check_answer(x, y, item[16+ii*2])
                     rt = rtClock.getTime()
                     if practice:
-                        Image("background", "arrow").buffer().draw()
-                        Image("questionMark", "questionMark").buffer().draw()
-                        Image(item[11+ii*2], "questionLoad").buffer().draw()
+                        load_background()
+                        Image("Question", "questionMark").buffer().draw()
+                        Image(item[15+ii*2], "questionLoad").buffer().draw()
                         if answer:
                             correct_answer_message.draw()
                         else:
@@ -336,39 +305,85 @@ def run_trials(items, practice=False):
         expWindow.flip()
         core.wait(1.5)  # 1500 ms
 
+# ===============================================================================
+# initial dialogue box
+# ===============================================================================
+inputInfo = ['a', '']
+count = 0
+while inputInfo[0] != inputInfo[1] or not inputInfo[0].isdigit():
+    expDlg = gui.Dlg(title="WM Experiment")
+    expDlg.addText("Subject info")
+    expDlg.addField("Subject_ID:")
+    expDlg.addField("Repeat__ID:")
+    expDlg.addText("")
+    if count:
+        expDlg.addText("ERROR: IDs must be same integers", color='Red')
+    expDlg.show()
+    count += 1
+    if expDlg.OK:
+        inputInfo = expDlg.data
+    else:
+        core.quit()
 
 # ===============================================================================
-# experiment
+# experiment preparation
 # ===============================================================================
+practice_list, header = read_stimuli('{0}\\stimuli\\Trials_Practice_WMExperiment.txt'.format(PATH))
+trials_list, header = read_stimuli('{0}\\stimuli\\Trials_WMExperiment.txt'.format(PATH))
+# Edit header line for using in the output
+header[0][0] = "trial"
+header[0][0:0] = ["Subject_ID", "load"]
+header[0].extend(["Answer1", "rt1", "Answer", "rt2", "Answer3", "rt3"])
+# output file
+expInfo = {'subjectID': inputInfo[1], 'expName': "WM_Experiment"}
+output_file = OUTPATH + expInfo['expName'] + '_%s.txt' % expInfo['subjectID']
+# create a window
+expWindow = visual.Window(size=SCREEN_SIZE, monitor="testMonitor", color=(230, 230, 230),
+                          colorSpace='rgb255', units=u'pix')
+# fullscr=True,
+# messages in practice part
+correct_answer_message = visual.TextStim(expWindow, pos=[0, 0], text="Richtig!", font='Courier New', bold=True,
+                                         color=(0, 1.0, 0), height=50, alignHoriz='center', units=u'pix')
+false_answer_message = visual.TextStim(expWindow, pos=[0, 0], text="Falsch!", font='Courier New', bold=True,
+                                       color=(1.0, 0, 0), height=50, alignHoriz='center', units=u'pix')
+time_up_message = visual.TextStim(expWindow, pos=[0, 0], text="Die Zeit ist um!", font='Courier New', bold=True,
+                                  color=(1.0, 0, 0), height=35, alignHoriz='center', units=u'pix')
 
 # ===============================================================================
-# instruction
+# instruction + practice_start_screen
 # ===============================================================================
-
 for im in range(20):
     Image("Intro" + str(im+1) + "_Murks", "arrow").buffer().draw()
-    expWindow.flip()  # flip blank screen
+    expWindow.flip()
     event.waitKeys(keyList=['space'])
+Image("practice_start_screen", "arrow").buffer().draw()
+expWindow.flip()  # flip blank screen
+event.waitKeys(keyList=['space'])
 
-# ------------------------------------------------------------------------------
-# practice part
-# run_trials(items, practice=True)
+# ===============================================================================
+# practice part + practice_start_screen
+# ===============================================================================
+run_trials(practice_list, practice=True)
+Image("practice_end_screen", "arrow").buffer().draw()
+expWindow.flip()  # flip blank screen
+event.waitKeys(keyList=['space'])
 
-# ------------------------------------------------------------------------------
-# experiment part
+# ===============================================================================
+# main part + Test_End_Screen
+# ===============================================================================
 with open(output_file, 'w') as o:
     o.write(";".join(header[0]) + "\n")
-    run_trials(trials_list)  # , practice=True)
-
-
-import pdb
-pdb.set_trace()
-
-# ------------------------------------------------------------------------------
-# finish screen
-
-expWindow.flip()
+    run_trials(trials_list)
+Image("Test_End_Screen", "arrow").buffer().draw()
+expWindow.flip()  # flip blank screen
 event.waitKeys(keyList=['space'])
 
 expWindow.close()
 core.quit()
+
+
+#expWindow.flip()
+#event.waitKeys(keyList=['space'])
+
+# expWindow.close()
+#core.quit()
